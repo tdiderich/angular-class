401d645b1d7693b2b027491e875ba43385c19029
tdiderich <tyler.diderich@jask.io>
2019-08-21 08:57:17 -0500

    made welcome page

--- angular-material/src/app/material.module.ts
+++ angular-material/src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
     MatCheckboxModule, 
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule
 } from "@angular/material";
 
 @NgModule({
@@ -23,7 +24,8 @@ import {
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule],
+        MatListModule,
+        MatTooltipModule],
     exports: [
         MatButtonModule, 
         MatIconModule, 
@@ -34,7 +36,8 @@ import {
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule]
+        MatListModule,
+        MatTooltipModule]
 }
 )
 export class MaterialModule{}
